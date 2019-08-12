9390cd9d5f0b73b66d76291643fea43b017852e3
Oluwaseunus <oluwaseunus@gmail.com>
2019-08-12 18:21:16 +0100

    Add first query and mutation resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
-
-module.exports = Mutations;
+module.exports = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+    return item;
+  }
+};
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+module.exports = {
+  /**
+   * Query
+   * @param {parent} - what makes the request (the parent schema)
+   * @param {args} - request arguments passed to the query
+   * @param {ctx} - the exposed database, and request params e.g., headers, cookies etc.
+   * @param {info} - information around the coming in graphql query
+   */
 
-module.exports = Query;
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items();
+    return items;
+  }
+};
